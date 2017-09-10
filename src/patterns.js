259ae124707ae770cf7339468a5fc876616b915b
Rob Rose <bertra97@gmail.com>
2017-09-09 20:09:06 -0400

    Made more upgrades, added onto options menu.
    - Rewrote some of the fields in the options.html for chrome.
    - Modified VS Code settings.json for project (also updated to latest
      version).
    - Updated manifest.
    - Reformatted patterns.js

--- src/patterns.js
+++ src/patterns.js
@@ -1,6 +1,15 @@
-const linkedInPattern = "^(([H|h][T|t][T|t][P|p][S|s]?:\/\/)?" + 
-                        "(?:[W|w]{3}\.)?([L|l][I|i][N|n][K|k][E|e][D|d][I|i][N|n].[C|c][O|o][M|m]\/([I|i][N|n]\/)?))?" + 
-                        "(?<usern>[A-Za-z0-9]{5,30})$";
-const githubPattern =   "^(([H|h][T|t][T|t][P|p][S|s]?:\/\/)?" + 
-                        "(?:[W|w]{3}\.)?([G|g][I|i][T|t][H|h][U|u][B|b]\.[C|c][O|o][M|m])\/)?" + 
-                        "(?<usern>[A-Za-z\d](?:[A-Za-z\d]|-(?=[A-Za-z\d])){0,38})$";
+const linkedInPatternStr = '' +
+  /^((https?:\/\/)?(?:www\.)?linkedin.com\/in\/)?/.source +
+  /(?<usern>[A-Za-z0-9]{5,30})$/.source;
+const githubPatternStr = '' +
+  /^((https?:\/\/)?(?:www\.)?github.com\/)?/.source +
+  /(?<usern>[A-Za-z\d](?:[A-Za-z\d]|-(?=[A-Za-z\d])){0,38})$/.source;
+const devpostPattern = '' +
+  /^((https?:\/\/)?(?:www\.)?devpost.com\/)?/.source +
+  /(?<usern>[A-Za-z\d_-]{1,40})$/.source;
+
+var linkedInRegExp = new RegExp(linkedInPatternStr, 'i');
+var githubRegExp = new RegExp(githubPatternStr, 'i');
+var devpostRegExp = new RegExp(devpostPattern, 'i');
+var urlRegExp = /^(([\w]+:)?\/\/)?(([\d\w]|%[a-fA-f\d]{2,2})+(:([\d\w]|%[a-fA-f\d]{2,2})+)?@)?([\d\w][-\d\w]{0,253}[\d\w]\.)+[\w]{2,63}(:[\d]+)?(/([-+_~.\d\w] |%[a - fA - f\d]{2,2}) *)*(\?(&?([-+_~.\d\w] |%[a - fA - f\d]{2,2}) =?)*)?(#([-+_~.\d\w] |%[a - fA - f\d]{2, 2 })*)?$ / i;
+var emailRegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
